7ff8db7ad3c77c1eba35c774d0a9fc5130908b2b
Ali Amirkafi <114346280+AliAK97@users.noreply.github.com>
2025-07-22 10:52:48 +0200

    Update inflammation-analysis.py

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -15,7 +15,7 @@ def main(args):
     """
     in_files = args.in_files
     if not isinstance(in_files, list):
-        in_files = [args.infiles]
+        in_files = [args.in_files]
 
 
     for filename in in_files:
